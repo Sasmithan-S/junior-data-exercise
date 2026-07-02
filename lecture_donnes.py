ee5becd9c0d944e40c9655ffd900c942ad888826
Sasmithan-S <satass438@gmail.com>
2026-07-02 09:57:18 +0200

    Lecture donnee brute des 4 CSV, avec header, suppression c_8 , escape)

--- lecture_donnes.py
+++ lecture_donnes.py
@@ -4,18 +4,34 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("aphp-pipeline").getOrCreate()
  
+
 #Pour eviter que Spark s'arrête au
 # premier "" qu'il trouve et coupe la ligne au mauvais endroit dès qu'il
-# rencontre une virgule ensuite , on ajoute escape = ' "" '
+# rencontre une virgule ensuite , on ajoute escape = ' " '
 
-patients = spark.read.csv("resources/patients.csv", header=True , escape='"')
+df_patients = spark.read.csv("resources/patients.csv", header=True , escape='"')
 
-#on supprime la colonne "_c8" du dataframe
-patients = patients.drop("_c8")
-print(patients.columns)
- 
-#  affichage  du  contenu 
-patients.show()
- 
+#on supprime la colonne "_c8" du data
+df_patients = df_patients.drop("_c8")
+
+#Lecture des 3 fichiers 
+df_identifiants_ipp = spark.read.csv("resources/identifiants_ipp.csv", header=True, escape='"')
+df_adresses = spark.read.csv("resources/adresses.csv", header=True, escape='"')
+df_opposition = spark.read.csv("resources/opposition_recherche.csv", header=True, escape='"') 
+
+print(df_patients.columns)
+
+df_patients.show()
+
+print(df_adresses.columns)
+df_adresses.show()
+
+print(df_identifiants_ipp.columns)
+df_identifiants_ipp.show()
+
+print(df_opposition.columns)
+df_opposition.show()
+
+
 spark.stop()
  

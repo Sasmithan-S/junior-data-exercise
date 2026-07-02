c08007727964cfb622d4ad73fcec526df8852fe1
Sasmithan-S <satass438@gmail.com>
2026-07-02 03:51:09 +0200

    reglage parsing '' et colonne c8

--- lecture_donnes.py
+++ lecture_donnes.py
@@ -1,20 +1,20 @@
 
 from pyspark.sql import SparkSession
  
-# On crée la session Spark, exactement comme dans test_spark.py.
+
 spark = SparkSession.builder.appName("aphp-pipeline").getOrCreate()
  
-# On lit le fichier patients.csv.
-# header=True dit à Spark : "la première ligne du fichier, ce n'est pas de
-# la donnée, ce sont les noms de colonnes". Sans ça, Spark inventerait des
-# noms génériques (_c0, _c1, _c2...) et traiterait ta ligne d'en-tête comme
-# si c'était un vrai patient.
-patients = spark.read.csv("resources/patients.csv", header=True)
- 
-# .columns : liste simplement les noms de colonnes que Spark a détectés.
+#Pour eviter que Spark s'arrête au
+# premier "" qu'il trouve et coupe la ligne au mauvais endroit dès qu'il
+# rencontre une virgule ensuite , on ajoute escape = ' "" '
+
+patients = spark.read.csv("resources/patients.csv", header=True , escape='"')
+
+#on supprime la colonne "_c8" du dataframe
+patients = patients.drop("_c8")
 print(patients.columns)
  
-# .show() : affiche le contenu sous forme de tableau dans le terminal.
+#  affichage  du  contenu 
 patients.show()
  
 spark.stop()
